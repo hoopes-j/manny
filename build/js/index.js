7f5e52f0718cb9a08e98817a5a1ffcbe1b69adc6
hoopes-j <hoopes.j@husky.neu.edu>
2021-06-04 21:29:47 -0400

    inject alerts

--- build/js/index.js
+++ build/js/index.js
@@ -1,15 +1,33 @@
 //import second from './second/second'
 function main() {
-    var sports = document.querySelector('.sports');
-    sports.addEventListener('click', function () {
-        window.location.pathname = '/enter';
-    });
+    console.log();
+    injectAlert();
 }
 ;
-// if (window.location.pathname = "") {
-//     main();
-// }
-// if (window.location.pathname = "./folder/second.ts") {
-//     second.main();
-// }
+var max = 50;
+var curNum = 10;
+function generateRandom() {
+    return Math.floor(Math.random() * max);
+}
+function injectAlert() {
+    curNum += 10;
+    var alertTemplate = "\n    <div class=\"manfreid-alert\" id=\"alert-" + curNum + "\">\n        <div class=\"manfreid-alert__header\">\n            <h2 class=\"manfreid-alert__header-title\">Warning</h2>\n            <button class=\"manfreid-alert__header-button\">X</button>\n        </div>\n        <div class=\"manfreid-alert__body\">\n            <div class=\"manfreid-alert__icon\"></div>\n            <div class=\"manfreid-alert__message\">\n                <img src=\"https://i.ibb.co/VB8ZBjp/computer.png\">\n                <div>\n                    <p>Welcome to Manny World.</p>\n                </div>\n            </div>\n            <div class=\"manfreid-alert__button-container\">\n                <button class=\"manfreid-alert__button-accept\">Enter</button>        \n                <button class=\"manfreid-alert__button-accept\">Also Enter</button>\n            </div>\n        </div>\n    </div>\n    ";
+    var newDiv = document.createElement('div');
+    newDiv.className = 'floating-alert';
+    newDiv.style.bottom = curNum + "px";
+    newDiv.style.left = curNum / 10 + 3 + "px";
+    newDiv.innerHTML = alertTemplate;
+    newDiv.querySelector('.manfreid-alert__header-button').addEventListener('click', function () {
+        window.location.assign('https://golf.com/');
+    });
+    var buttons = newDiv.querySelectorAll('.manfreid-alert__button-accept');
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function () {
+            injectAlert();
+        });
+    }
+    var parent = document.getElementById('alertContainer');
+    parent.append(newDiv);
+}
+main();
 //# sourceMappingURL=index.js.map
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,19 +2,63 @@
 
 
 function main() {
-    const sports: HTMLElement = document.querySelector('.sports');
 
-    sports.addEventListener('click', () => {
-        window.location.pathname = '/enter';
-    })
+    console.log()
+
+    injectAlert();
 
 };
 
+const max = 50;
+var curNum = 10;
 
-// if (window.location.pathname = "") {
-//     main();
-// }
+function generateRandom() {
+    return Math.floor(Math.random() * max);
+}
 
-// if (window.location.pathname = "./folder/second.ts") {
-//     second.main();
-// }
+function injectAlert() {
+    curNum += 10;
+    const alertTemplate = `
+    <div class="manfreid-alert" id="alert-${curNum}">
+        <div class="manfreid-alert__header">
+            <h2 class="manfreid-alert__header-title">Warning</h2>
+            <button class="manfreid-alert__header-button">X</button>
+        </div>
+        <div class="manfreid-alert__body">
+            <div class="manfreid-alert__icon"></div>
+            <div class="manfreid-alert__message">
+                <img src="https://i.ibb.co/VB8ZBjp/computer.png">
+                <div>
+                    <p>Welcome to Manny World.</p>
+                </div>
+            </div>
+            <div class="manfreid-alert__button-container">
+                <button class="manfreid-alert__button-accept">Enter</button>        
+                <button class="manfreid-alert__button-accept">Also Enter</button>
+            </div>
+        </div>
+    </div>
+    `;
+    const newDiv = document.createElement('div');
+    newDiv.className = 'floating-alert';
+    newDiv.style.bottom = `${curNum}px`;
+    newDiv.style.left = `${curNum/10 + 3}px`; 
+    newDiv.innerHTML = alertTemplate;
+    newDiv.querySelector('.manfreid-alert__header-button').addEventListener('click', () => {
+        window.location.assign('https://golf.com/');
+    })
+    const buttons = newDiv.querySelectorAll('.manfreid-alert__button-accept');
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', () => {
+           injectAlert();
+        })
+    }
+    const parent = document.getElementById('alertContainer');
+    parent.append(newDiv);
+}
+
+
+
+main();
+
+
